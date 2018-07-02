a7c32ab3194e12ba0fe41711db825e4962e5f9e5
Apoorva Singh <apoorva.singh2810@gmail.com>
2018-07-03 01:25:54 +0530

    Basic Line Chart - Completed

--- app/src/main/java/com/apoorvasingh2810/linechartapp/MainActivity.java
+++ app/src/main/java/com/apoorvasingh2810/linechartapp/MainActivity.java
@@ -2,12 +2,62 @@ package com.apoorvasingh2810.linechartapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.listener.ChartTouchListener;
+import com.github.mikephil.charting.listener.OnChartGestureListener;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG= "MainActivity";
+
+    private LineChart mChart;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    mChart=(LineChart)findViewById(R.id.lineChart);
+
+//    mChart.setOnChartGestureListener(MainActivity.this);
+//    mChart.setOnChartValueSelectedListener(MainActivity.this);
+
+    mChart.setDragEnabled(true);
+    mChart.setScaleEnabled(false);
+
+        ArrayList<Entry> yValues= new ArrayList<>();
+
+        yValues.add(new Entry(0,99f));
+        yValues.add(new Entry(1,19f));
+        yValues.add(new Entry(2,66f));
+        yValues.add(new Entry(3,44f));
+        yValues.add(new Entry(4,66f));
+        yValues.add(new Entry(5,7f));
+        yValues.add(new Entry(6,50f));
+
+        LineDataSet set1=new LineDataSet(yValues, "Data Set 1");
+
+        set1.setFillAlpha(110);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+
+        mChart.setData(data);
+
     }
+
+
 }
