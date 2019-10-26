ef8282d535aa7e9504362811f6ad9fbfa238e00a
lite zhang <lite@wirush.ai>
2019-10-26 14:34:22 +0800

    for demo  ver-0.1

---
+++ app/src/main/java/com/apoorvasingh2810/linechartapp/ChartUtils.java
@@ -0,0 +1,309 @@
+package com.apoorvasingh2810.linechartapp;
+
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * @author lite.zhang
+ * @name Line-Chart-Android_App
+ * @class name：com.apoorvasingh2810.linechartapp
+ * @class describe
+ * @time 2019/10/26 11:34
+ */
+
+public class ChartUtils {
+
+    public static int dayValue = 0;
+    public static int weekValue = 1;
+    public static int monthValue = 2;
+    public static int count = 0;
+
+    public static int brokenLine = 100;
+    public static int curve = 200;
+    public static int mLineType = brokenLine;
+
+    /**
+     * 初始化图表
+     *
+     * @param chart 原始图表
+     * @return 初始化后的图表
+     */
+    public static LineChart initChart(LineChart chart) {
+        // 不显示数据描述
+        chart.getDescription().setEnabled(false);
+        // 没有数据的时候，显示“暂无数据”
+        chart.setNoDataText("无加载数据");
+        // 不显示表格颜色
+        chart.setDrawGridBackground(true);
+        // 不可以缩放
+        chart.setScaleEnabled(false);
+        // 不显示y轴右边的值
+        chart.getAxisRight().setEnabled(false);
+        chart.setDrawBorders(true);
+        // 不显示图例
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+        // 向左偏移15dp，抵消y轴向右偏移的30dp
+        chart.setExtraLeftOffset(-15);
+
+        XAxis xAxis = chart.getXAxis();
+        // 不显示x轴
+        xAxis.setDrawAxisLine(true);
+        // 设置x轴数据的位置
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextColor(Color.WHITE);
+        xAxis.setTextSize(12);
+        xAxis.setGridColor(Color.parseColor("#30FFFFFF"));
+//        xAxis.setDrawGridLines(false);
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        xAxis.setEnabled(false);
+        // 设置x轴数据偏移量
+        xAxis.setYOffset(-12);
+        xAxis.setAxisMaximum(120f);
+        xAxis.setDrawGridLines(true);
+        xAxis.setLabelCount(8);
+
+        YAxis yAxis = chart.getAxisLeft();
+        // 不显示y轴
+        yAxis.setDrawAxisLine(true);
+        // 设置y轴数据的位置
+        yAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        // 不从y轴发出横向直线
+        yAxis.setDrawGridLines(true);
+        yAxis.setTextColor(Color.WHITE);
+        yAxis.setTextSize(12);
+        // 设置y轴数据偏移量
+        yAxis.setXOffset(30);
+        yAxis.setYOffset(-3);
+        yAxis.setAxisMinimum(0);
+        yAxis.enableGridDashedLine(10f, 10f, 0f);
+        yAxis.setEnabled(false);
+
+        xAxis.setAxisLineColor(Color.WHITE);//设置x轴线颜色
+        xAxis.setAxisLineWidth(0.5f);//设置x轴线宽度
+        YAxis leftAxis = chart.getAxisLeft();
+        YAxis axisRight = chart.getAxisRight();
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);  //设置Y轴网格线条的虚线，参1 实线长度，参2 虚线长度 ，参3 周期
+        leftAxis.setGranularity(20f); // 网格线条间距
+        axisRight.setEnabled(false);   //设置是否使用 Y轴右边的
+        leftAxis.setEnabled(true);     //设置是否使用 Y轴左边的
+        leftAxis.setGridColor(Color.parseColor("#7189a9"));  //网格线条的颜色
+        leftAxis.setDrawLabels(true);        //是否显示Y轴刻度
+        leftAxis.setStartAtZero(true);        //设置Y轴数值 从零开始
+        leftAxis.setDrawGridLines(true);      //是否使用 Y轴网格线条
+        leftAxis.setTextSize(12f);            //设置Y轴刻度字体
+        leftAxis.setTextColor(Color.WHITE);   //设置字体颜色
+        leftAxis.setAxisLineColor(Color.WHITE); //设置Y轴颜色
+        leftAxis.setAxisLineWidth(0.5f);
+        leftAxis.setDrawAxisLine(true);//是否绘制轴线
+
+        Matrix matrix = new Matrix();
+        //x轴缩放1.5倍
+        matrix.postScale(1.5f, 1f);
+        //在图表动画显示之前进行缩放
+        chart.getViewPortHandler().refresh(matrix, chart, false);
+        //x轴执行动画
+        chart.animateX(2000);
+        chart.invalidate();
+        return chart;
+    }
+
+    /**
+     * 设置图表数据
+     *
+     * @param chart  图表
+     * @param values 数据
+     */
+    public static void setChartData(LineChart chart, List<Entry> values) {
+        LineDataSet lineDataSet;
+
+        if (chart.getData() != null && chart.getData().getDataSetCount() > 0) {
+            lineDataSet = (LineDataSet) chart.getData().getDataSetByIndex(0);
+            lineDataSet.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+            //设置在曲线图中显示的最大数量
+            chart.setVisibleXRangeMaximum(10);
+            //移到某个位置
+            chart.moveViewToX(chart.getData().getEntryCount() - 5);
+        } else {
+            lineDataSet = new LineDataSet(values, "");
+            // 设置曲线颜色
+            lineDataSet.setColor(Color.parseColor("#FFFFFF"));
+            // 设置平滑曲线
+//            lineDataSet.setMode(LineDataSet.Mode.CUBIC_BEZIER);
+            // 不显示坐标点的小圆点
+            lineDataSet.setDrawCircles(false);
+            // 不显示坐标点的数据
+            lineDataSet.setDrawValues(false);
+            lineDataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
+            // 不显示定位线
+            lineDataSet.setHighlightEnabled(true);
+
+            LineData data = new LineData(lineDataSet);
+            chart.setData(data);
+            chart.invalidate();
+        }
+    }
+
+    /**
+     * 设置线条填充背景颜色
+     *
+     * @param drawable
+     */
+    public static void setChartFillDrawable(LineChart chart, Drawable drawable) {
+        if (chart.getData() != null && chart.getData().getDataSetCount() > 0) {
+            LineDataSet lineDataSet = (LineDataSet) chart.getData().getDataSetByIndex(0);
+            //避免在 initLineDataSet()方法中 设置了 lineDataSet.setDrawFilled(false); 而无法实现效果
+            lineDataSet.setDrawFilled(true);
+            lineDataSet.setFillDrawable(drawable);
+            chart.invalidate();
+        }
+    }
+
+    public static LineChart initNewChart(LineChart chart) {
+        chart.setDragEnabled(true);
+        chart.setScaleEnabled(false);
+        chart.getDescription().setEnabled(false);
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // 设置X轴的位置
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.RED);
+        xAxis.setDrawAxisLine(true);
+        xAxis.setDrawGridLines(true);
+        Matrix matrix = new Matrix();
+//        //x轴缩放1.5倍
+//        matrix.postScale(1.5f, 1f);
+//        //在图表动画显示之前进行缩放
+//        chart.getViewPortHandler().refresh(matrix, chart, false);
+        //x轴执行动画
+        chart.animateX(1000);
+        chart.invalidate();
+        return chart;
+    }
+
+    public static void setRPMChartData(LineChart chart, List<Entry> rpm, List<Entry> avgRpm) {
+        LineDataSet rpmDataSet = new LineDataSet(rpm, "RPM");
+        LineDataSet avgRpmDataSet = new LineDataSet(avgRpm, "AVG_RPM");
+        rpmDataSet.setFillAlpha(110);
+        avgRpmDataSet.setFillAlpha(110);
+
+        rpmDataSet.setColor(Color.parseColor("#1F77B4"));
+        avgRpmDataSet.setColor(Color.parseColor("#FF7F0E"));
+
+        rpmDataSet.setLineWidth(1f);
+        rpmDataSet.setValueTextSize(12f);
+        rpmDataSet.setValueTextColor(Color.BLACK);
+
+        rpmDataSet.setDrawCircles(false);
+        // 不显示坐标点的数据
+        rpmDataSet.setDrawValues(false);
+        rpmDataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
+        // 不显示定位线
+        rpmDataSet.setHighlightEnabled(true);
+
+        avgRpmDataSet.setDrawCircles(false);
+        avgRpmDataSet.setDrawValues(false);
+        avgRpmDataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
+        // 不显示定位线
+        avgRpmDataSet.setHighlightEnabled(true);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(rpmDataSet);
+        dataSets.add(avgRpmDataSet);
+
+        LineData data = new LineData(dataSets);
+        chart.setData(data);
+    }
+
+    /**
+     * 更新图表
+     *
+     * @param chart     图表
+     * @param values    数据
+     */
+    public static void notifyDataSetChanged(LineChart chart, List<Entry> values,
+                                            final int lineType) {
+
+        chart.invalidate();
+        setChartData(chart, values);
+    }
+
+    /**
+     * 更新图表
+     *
+     * @param chart  图表
+     * @param values 数据
+     * @param xValue x轴刻度
+     */
+    public static void notifyDataSetChanged(LineChart chart, List<Entry> values,
+                                            final String[] xValue) {
+
+        chart.getXAxis().setValueFormatter(new IAxisValueFormatter() {
+
+            @Override
+            public String getFormattedValue(float value, AxisBase axis) {
+                count++;
+                Log.i("lite", " count  " + count);
+                Log.i("lite", " xValue len  " + xValue.length);
+                for (int i = 0; i < xValue.length; i++) {
+                    Log.i("lite", " xValue [" + i + "] " + xValue[i]);
+                }
+                return xValue[(int) value];
+            }
+        });
+
+        chart.invalidate();
+        setChartData(chart, values);
+    }
+
+    public static void setBPMChartData(LineChart chart, List<Entry> bpm) {
+        LineDataSet bpmDataSet = new LineDataSet(bpm, "BPM");
+//        LineDataSet avgRpmDataSet = new LineDataSet(avgRpm, "AVG_RPM");
+        bpmDataSet.setFillAlpha(110);
+//        avgRpmDataSet.setFillAlpha(110);
+
+        bpmDataSet.setColor(Color.parseColor("#1F77B4"));
+//        avgRpmDataSet.setColor(Color.parseColor("#FF7F0E"));
+
+        bpmDataSet.setLineWidth(1f);
+        bpmDataSet.setValueTextSize(12f);
+        bpmDataSet.setValueTextColor(Color.BLACK);
+
+        bpmDataSet.setDrawCircles(false);
+        // 不显示坐标点的数据
+        bpmDataSet.setDrawValues(false);
+        bpmDataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
+        // 不显示定位线
+        bpmDataSet.setHighlightEnabled(true);
+
+//        avgRpmDataSet.setDrawCircles(false);
+//        avgRpmDataSet.setDrawValues(false);
+//        avgRpmDataSet.setValueTextColor(Color.parseColor("#FFFFFF"));
+        // 不显示定位线
+//        avgRpmDataSet.setHighlightEnabled(true);
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(bpmDataSet);
+//        dataSets.add(avgRpmDataSet);
+
+        LineData data = new LineData(dataSets);
+        chart.setData(data);
+    }
+}
+
--- app/src/main/java/com/apoorvasingh2810/linechartapp/MainActivity.java
+++ app/src/main/java/com/apoorvasingh2810/linechartapp/MainActivity.java
@@ -1,83 +1,303 @@
 package com.apoorvasingh2810.linechartapp;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
-import com.github.mikephil.charting.listener.ChartTouchListener;
-import com.github.mikephil.charting.listener.OnChartGestureListener;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG= "MainActivity";
+    private static final String TAG = "MainActivity";
 
-    private LineChart mChart;
+    private LineChart mRpmChart;
+    private LineChart mBpmChart;
+    private LineChart mApmChart;
+    private LineChart mRpmPatternChart;
+    private LineChart mFastSlowChart;
 
     Button btnSecondActivity;
+
+    float[] RPM =  new float[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            26.88f,
+            23.65f,
+            21.50f,
+            21.50f,
+            19.35f,
+            18.27f,
+            15.05f,
+            12.90f,
+            12.90f,
+            12.90f,
+            12.90f,
+            12.90f,
+            11.82f,
+            10.75f,
+            9.67f,
+            9.67f,
+            7.52f,
+            7.52f,
+            7.52f
+    };
+
+    float[] AVG_RPM = new float[]{
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            6.0f,
+            6.0f,
+            6.0f,
+            8.25f,
+            9.6f,
+            10.5f,
+            11.0f,
+            11.5f,
+            12.0f,
+            11.5f,
+            11.0f,
+            10.5f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f,
+            0.0f
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    mChart=(LineChart)findViewById(R.id.lineChart);
+        mRpmChart = findViewById(R.id.rpm_chart);
+        mBpmChart = findViewById(R.id.bpm_chart);
+        mApmChart = findViewById(R.id.apm_chart);
+        mRpmPatternChart = findViewById(R.id.rpm_pattern_chart);
+        mFastSlowChart = findViewById(R.id.fast_slow_chart);
 
-    btnSecondActivity=(Button)findViewById(R.id.btnSecondActivity);
+        btnSecondActivity = findViewById(R.id.btnSecondActivity);
 
         btnSecondActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
         });
-    //    mChart.setOnChartGestureListener(MainActivity.this);
-//    mChart.setOnChartValueSelectedListener(MainActivity.this);
+//    mRpmChart.setOnChartGestureListener(MainActivity.this);
+//    mRpmChart.setOnChartValueSelectedListener(MainActivity.this);
+//        ChartUtils.initChart(mRpmChart);
+//        mRpmChart.setDragEnabled(true);
+//        mRpmChart.setScaleEnabled(false);
+//        mRpmChart.getDescription().setEnabled(false);
+//        XAxis xAxis = mRpmChart.getXAxis();
+//        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // 设置X轴的位置
+//        xAxis.setTextSize(10f);
+//        xAxis.setTextColor(Color.RED);
+//        xAxis.setDrawAxisLine(true);
+//        xAxis.setDrawGridLines(true);
 
-    mChart.setDragEnabled(true);
-    mChart.setScaleEnabled(false);
+        ChartUtils.initNewChart(mRpmChart);
 
-        ArrayList<Entry> yValues= new ArrayList<>();
+        ArrayList<Entry> rpmValues = new ArrayList<>();
 
-        yValues.add(new Entry(0,99f));
-        yValues.add(new Entry(1,19f));
-        yValues.add(new Entry(2,66f));
-        yValues.add(new Entry(3,44f));
-        yValues.add(new Entry(4,66f));
-        yValues.add(new Entry(5,7f));
-        yValues.add(new Entry(6,50f));
+        for (int i = 0; i< RPM.length; i++) {
+            rpmValues.add(new Entry(i, RPM[i]));
+        }
 
-        LineDataSet set1=new LineDataSet(yValues, "Data Set 1");
-
-        set1.setFillAlpha(110);
-        set1.setColor(Color.CYAN);  //Changing Color of Line
-        set1.setLineWidth(3f);
-        set1.setValueTextSize(12f);
-        set1.setValueTextColor(Color.BLACK);
-
-
-
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
-        dataSets.add(set1);
-
-        LineData data = new LineData(dataSets);
-
-        mChart.setData(data);
+        ArrayList<Entry> avgRpmValues = new ArrayList<>();
+        for (int i = 0; i< AVG_RPM.length; i++) {
+            avgRpmValues.add(new Entry(i, AVG_RPM[i]));
+        }
+        ChartUtils.setRPMChartData(mRpmChart, rpmValues, avgRpmValues);
 
     }
-
-
 }
